df8a0cc55d4bf2908d0eaf74859304fe7c464a38
Derrick Huynh <derrickhuynh56@gmail.com>
2021-10-10 03:21:38 +0100

    add clientTest.py

---
+++ Lab3/clientTest.py
@@ -0,0 +1,11 @@
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostname()
+host_ip = socket.gethostbyname(host)
+port = 8080
+client.connect((host_ip, 8080))
+client.send('I am CLIENT'.encode())
+from_server = client.recv(4096)
+client.close()
+print(from_server.decode('ascii'))
